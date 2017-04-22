84af2412f27d8c84dbbf757dd36ccc208bc81e6d
Doidjin <LeonardoCabbau@MacBook-Air-de-LEONARDO.local>
2017-04-22 15:58:26 -0300

    Variáveis e operadores

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,3 +1,34 @@
-var titulo = document.querySelector(".titulo");
+var paciente = document.querySelector('#primeiro-paciente');
 
-titulo.textContent = "Aparecida Nutricionista";
+var tdPeso = paciente.querySelector('.info-peso');
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector('.info-altura');
+var altura = tdAltura.textContent;
+
+var tdImc = document.querySelector('.info-imc');
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido");
+    pesoEhValido = false;
+    tdImc.textContent = 'Peso Inválido';
+}
+
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida");
+    alturaEhValida = false;
+    tdImc.textContent = 'Altura Inválida';
+}
+
+if(pesoEhValido && alturaEhValida){
+    var imc = peso / (altura * altura);
+
+    tdImc.textContent = imc;
+}
+
+
+
+
